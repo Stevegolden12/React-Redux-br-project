aca8bb25b0f33f061a3b72bf736029460df87931
Steve Golden <sgolden2525@outlook.com>
2018-10-17 13:01:48 -0600

    Finishing up
    Added user-details.js file in the containers folder.
    
    Added a stateful component within the user-details called userDetail
    
    Added mapStateToProps function as well to userDetail
    
    Added the userDetail stateful component to the app.js so it will render

--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import UserList from '../containers/user-list'
+import UserDetail from '../containers/users-detail'
 require('../../scss/style.scss);
+ 
 
 const App = () => (
   <div>
 		<h2>Username List:</h2>
 		<Userlist />
 		<hr />
-		<h2>User Details</h2>
+    <h2>User Details</h2>
+    <UserDetail />
 	</div>
 );
 
